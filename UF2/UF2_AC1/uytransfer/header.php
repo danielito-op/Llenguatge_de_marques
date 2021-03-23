9c17a747539c1cab901c12ab44b34b5e7cbd3b09
danielito-op <daniel.foix@mataro.epiaedu.cat>
2021-03-23 12:33:14 +0100

    Creant el header.php

---
+++ UF2/UF2_AC1/uytransfer/header.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>index</title>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+    </head>
+
+    <body>
+    	<form name="datos" action="upload.php" method="post" enctype="multipart/form-data">
+    <?php
+    
+    echo "<table width=\"100%\" bordercolor=\"#FA5858\" bgcolor=\"#FA5858\">
+    <tr>
+   <td width=\"75%\"><a href=\"https://www.google.com\">Uy!Transfer</a></td>
+    <td><a href=\"upload.php\">Enviar archivo</a></td>
+    <td><a href=\"https://www.google.com\">Mis ultimos archivos</a></td>
+    <tr>
+    </table>";
+
+ 	?>
+    </body>
+</html>
